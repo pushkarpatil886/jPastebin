5e72626fd52fb6dc72661e6c6750427dfb88daa3
pushkar4820 <patilbrand4820@gmail.com>
2024-04-26 14:49:56 +0530

    paste example of java

--- examples/SimplePasteExample.java
+++ examples/SimplePasteExample.java
@@ -1,13 +1,7 @@
 import org.jpaste.exceptions.PasteException;
 import org.jpaste.pastebin.Pastebin;
 
-/**
- * 
- * Example on how to generate a simple paste
- * 
- * @author Brian B
- *
- */
+
 public class SimplePasteExample {
 	
 	public static void main(String[] args) throws PasteException {
@@ -15,7 +9,7 @@ public class SimplePasteExample {
 		String title = "My first jPastebin paste!"; // insert your own title
 		String contents = "Hello world"; // insert your own paste contents
 		
-		// paste, get URL & print
+		
 		System.out.println(Pastebin.pastePaste(developerKey, contents, title));
 	}
 
